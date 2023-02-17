cc848f60f5903dbcf979641a360d526645e4d86f
Rahul Bhagat <rahulbhagat13slr@gmail.com>
2023-02-17 22:01:18 +0530

    Basic UI

---
+++ components/Banner.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image"
+import { useEffect, useState } from "react"
+import { Movie } from "../typings"
+import { baseUrl } from "../constants/movie";
+interface Props{
+    netflixOriginals : Movie[]
+}
+function Banner({ netflixOriginals }: Props) {
+    const [movie, setMovie] = useState<Movie | null>(null)
+    useEffect(() => {
+      setMovie(
+        netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+      )
+    }, [netflixOriginals])
+
+    return (
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
+        <Image
+                    layout="fill"
+                    src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+                    objectFit="cover" alt={""}        />
+      </div>
+
+      <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+        {movie?.overview}
+      </p>
+      <div className="flex space-x-3">
+        <button className="bannerButton bg-white text-black">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-4 w-4 text-black md:h-7 md:w-7">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
+</svg>
+          Play
+        </button>
+
+        <button
+          className="bannerButton bg-[gray]/70"
+          onClick={() => {
+            
+          }}
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-5 w-5 md:h-8 md:w-8">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+</svg>
+
+ More Info
+        </button>
+      </div>
+    </div>
+  )}
+export default Banner
---
+++ components/Header.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link"
+import { useEffect, useState } from "react";
+// import {SearchIcon} from '@heroicons/react/solid';
+function Header(){
+  const [isScrolled,setisScrolled] = useState(false);
+  useEffect(() =>{
+    const handleScroll=()=>{
+      if(window.scrollY > 0){
+        setisScrolled(true);
+      }
+      else{
+        setisScrolled(false);
+      }
+    }
+    window.addEventListener("scroll" , handleScroll)
+    return()=>{
+      window.removeEventListener('scroll' , handleScroll)
+    }
+
+  },[])
+    return(
+     <header className="`${isScrolled && 'bg-red-500'}` }">
+         <div className="flex items-center space-x-2 md:space-x-10">
+         <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+          <ul className="hidden space-x-4 md:flex">
+            <li className="headerLink">Home</li>
+            <li className="headerLink">TV Shows</li>
+            <li className="headerLink">Movies</li>
+            <li className="headerLink">My List</li>
+            <li className="headerLink">New & Popular</li>
+          </ul>
+        </div>
+        <div className="flex item-center space-x-4 text-sm font-light">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 sm:inline" viewBox="0 0 20 20" fill="currentColor">
+  <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
+</svg>
+<p className="hidden lg:inline">Kids</p>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path fill-rule="evenodd" d="M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z" clip-rule="evenodd" />
+</svg>
+<Link href='/account'>
+<img src="https://rb.gy/g1pwyx" alt="" className="cursor-pointer rounded"/>
+</Link>
+
+        </div>
+     </header>
+    )
+}
+export default Header
